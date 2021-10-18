ed91eaacd0f946f847fd138abf393b7ece4a65c5
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-18 19:08:58 +0300

    exercise 5.2.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,6 +31,10 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user)
+      )
 
       blogService.setToken(user.token)
 
@@ -32,6 +45,11 @@ const App = () => {
       window.alert('Wrong credentials!')
     }
 
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBloglistUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -68,7 +86,8 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in <button onClick={ handleLogout }>Logout</button></p>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
