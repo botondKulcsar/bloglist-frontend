15856d5add4d1de13c1be7ae6f519d6fdaa20f9e
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-19 11:24:30 +0300

    exercise 5.7.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false)
+
+  // console.log(blog?.user?.name)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const changeDetails = () => setDetails(!details)
+
+  return (
+    <div style={blogStyle}>
+      {
+        details
+          ? <div>
+            {blog.title} {blog.author}
+              <button onClick={changeDetails}>hide</button><br />
+            {blog.url}<br />
+            likes {blog.likes} <button>like</button><br />
+            {blog?.user?.name ?? ''}
+          </div>
+          : <div>
+            {blog.title} {blog.author}
+            <button onClick={changeDetails}>view</button>
+          </div>
+      }
+
+    </div>
+  )
+}
 
 export default Blog
